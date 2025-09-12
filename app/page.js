38a683bc7b01e592a84395c013862ca2ffd199ed
Nirob Sarker <nirobsarker012@gmail.com>
2025-09-13 01:25:54 +0600

    add: navbar

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Navbar from "./src/components/Navbar/Navbar";
 
 export default function Home() {
--- app/src/components/Navbar/Navbar.jsx
+++ app/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Image from "next/image";
+import logo from "../../assets/Logo/Logo.svg";
 
 const Navbar = () => {
-  return <div>Hello World</div>;
+  return (
+    <header className="container">
+      <nav className="flex items-center justify-center pt-[30px] pb-[80px]">
+        <Image src={logo} alt="ena-ema" className="w-[120px]" />
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
